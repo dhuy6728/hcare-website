7187aa871a0e7d86f4382eb13b2c69b5f3c9ca98
Hcare <thend6728@gmail.com>
2026-09-11 16:50:51 +0700

    Initial commit

---
+++ js/global.js
@@ -0,0 +1,17 @@
+/* LOAD COMPONENTS */
+document.addEventListener("DOMContentLoaded", function() {
+    
+    fetch('components/navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar-placeholder').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading navbar:', error));
+
+    fetch('components/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer-placeholder').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading footer:', error));
+});
